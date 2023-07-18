dfa9d7b235d6333bb478d63ef4a9e38f875fe8cf
Macbook air <sammystradling2006@gmail.com>
2023-07-18 10:19:56 +0100

    working, but limits for api calls per minute. Hence new branch to track fmp library usage experiment

--- api/main.py
+++ api/main.py
@@ -2,34 +2,34 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
-from companies import companies
+from companies import company_dictionary
 
 def get_data(companies: dict, daterange:int):
 
 
     load_dotenv()
     api_key = os.environ.get("api-token")
-
-    symbol = "AAPL"
-    interval = "1day" 
-    outputSize = "3" # how many days it goes back
-
-
-    # 1096 days needed for 3 years
+    for key in companies.keys():
+        symbol = key
+        interval = "1day" 
+        outputSize = daterange # how many days it goes back
 
 
-    url = f"https://api.twelvedata.com/avgprice?symbol={symbol}&interval={interval}&outputsize={outputSize}&apikey={api_key}"
+        # 1096 days needed for 3 years
 
-    # THIS WILL HAVE SOME MISSING DATA, FOR NON TRADING DAYS SUCH AS WEEKENDS AND BANK HOLIDAYS. 
-    # HENCE, AUTOFILL THESE DATES WITH THE LAST DATA SET AVAILABLE
 
-    data = requests.get(url).json()
+        url = f"https://api.twelvedata.com/avgprice?symbol={symbol}&interval={interval}&outputsize={outputSize}&apikey={api_key}"
 
-    print(json.dumps(data, indent=4))
+        # THIS WILL HAVE SOME MISSING DATA, FOR NON TRADING DAYS SUCH AS WEEKENDS AND BANK HOLIDAYS. 
+        # HENCE, AUTOFILL THESE DATES WITH THE LAST DATA SET AVAILABLE
+
+        data = requests.get(url).json()
+
+        print(json.dumps(data, indent=4))
 
 
 def main():
-    get_data(companies, 3)
+    get_data(company_dictionary, 3)
 
 if __name__ == "__main__":
     main()
