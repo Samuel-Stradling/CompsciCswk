4d73c3acb2a7dd4371ca0e2746a1878c181b5184
Macbook air <sammystradling2006@gmail.com>
2023-07-19 09:39:46 +0100

    refactored - functions for each task. Deleting in cleanse data not fully working yet

--- api/main.py
+++ api/main.py
@@ -1,24 +1,37 @@
-import requests
 import json
-import os
-from dotenv import load_dotenv
 from companies import company_dictionary
-#
-def get_data(companies: dict, dateRange:int):
 
-
+def call_api(company: str, dateRange: int) -> json:
+    import requests
+    import os
+    from dotenv import load_dotenv
     load_dotenv()
     api_key = os.environ.get("api-token")
-    for company in companies:
-        url = f'https://financialmodelingprep.com/api/v3/historical-price-full/{company}?timeseries={dateRange}&apikey={api_key}'
-        print(requests.get(url).json())
+    url = f'https://financialmodelingprep.com/api/v3/historical-price-full/{company}?timeseries={dateRange}&apikey={api_key}'
+    return requests.get(url).json()
+
+def cleanse_data(originalIn: json) -> json:
+    dirtyData = originalIn.copy()
+
+    # for key in dirtyData:
+    #     print(key)
+
+    del dirtyData["historical"]["open"]
+    del dirtyData["high"]
+    del dirtyData["low"]
+    del dirtyData["unadjustedVolume"]
+    del dirtyData["vwap"]
+    del dirtyData["label"]
+    del dirtyData["changeOverTime"]
+
+    return dirtyData
+
+a = cleanse_data(call_api("AAPL", 4))
 
 
-    
 
 
-def main():
-    get_data(company_dictionary, 3)
 
-if __name__ == "__main__":
-    main()
+
+
+
