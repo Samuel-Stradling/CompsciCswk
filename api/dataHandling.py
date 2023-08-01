ff23ed82058f5cf46c3ce3d08dba37392c7e10d3
Sam Lee <sammystradling2006@gmail.com>
2023-08-01 22:22:54 +0100

    add call_ticker_current function to get the current price for a specific company. Replaced the dict keys in cleanse data with more descriptive names. added comments

--- api/dataHandling.py
+++ api/dataHandling.py
@@ -1,3 +1,8 @@
+# The raw data from the api follows this format: {key:value, key:value, results:[ticker:"APPL", ...]}
+
+# The cleansed data is as follows: [date, {ticker:"AAPL", ...}]
+
+
 import requests
 
 
@@ -19,29 +24,80 @@ def call_all_companies(date: str) -> list:
     companySortedData = [date]
     while counter < len(rawData["results"]):
         subDictionary = rawData["results"][counter]
-        if subDictionary["T"] in company_dictionary:
+        if (
+            subDictionary["T"] in company_dictionary
+        ):  # rawData contains many more companies then we need hence...
             companySortedData.append(subDictionary)
         counter += 1
 
     return companySortedData
 
 
-def call_ticker(ticker: str, date: str):
-    # this will need web-scraping from: https://polygon.io/quote/{ticker}
-    response = requests.get("https://polygon.io/quote/AAPL").text
-    print(response)
+def call_ticker_current(ticker: str) -> list:
+    from bs4 import BeautifulSoup
+    import re
+    import datetime
+    import json
+
+    finalData = [(str(datetime.datetime.now())[:10]), {}]
+    finalData[1]["ticker"] = ticker
+
+    response = requests.get(f"https://polygon.io/quote/{ticker}")
+
+    if response.status_code == 200:
+        responseContents = response.text
+        soup = BeautifulSoup(responseContents, "html.parser")
+
+        currentPriceElement = soup.find(
+            "title"
+        )  # the current price is found from the title of the webpage, along with percentage change from open
+        if currentPriceElement:
+            currentPriceRaw = currentPriceElement.text.strip()
+            pattern = r"(-\d+\.\d{2}%|\+\d+\.\d{2}%) (\d+\.\d+)"  # first group is for the percentage, accounting for a +ve & -ve. second group for current price
+            currentPercentageChange = re.search(pattern, currentPriceRaw).group(1)
+            currentPriceValue = float(re.search(pattern, currentPriceRaw).group(2))
+
+        data = soup.find(
+            id="__NEXT_DATA__"
+        ).text  # data is held within a json inside a script tag in the html
+        data = json.loads(data)
+        currentOpen = data["props"]["pageProps"]["open"]
+        prevClose = data["props"]["pageProps"]["close"]
+        currentHigh = data["props"]["pageProps"]["high"]
+        currentLow = data["props"]["pageProps"]["low"]
+        currentVolume = data["props"]["pageProps"]["volume"]
+
+        finalData[1]["currentOpen"] = currentOpen
+        finalData[1]["currentHigh"] = currentHigh
+        finalData[1]["currentLow"] = currentLow
+        finalData[1]["currentVolume"] = currentVolume
+        finalData[1]["prevClose"] = prevClose
+        finalData[1]["currentPrice"] = currentPriceValue
+        finalData[1]["currentPercentageChange"] = currentPercentageChange
+
+        return finalData
+
+    else:
+        raise NameError(f"Provided ticker yielded {response.status_code} response")
 
 
 def cleanse_data(originalIn: list) -> list:
     for index, dictionary in enumerate(originalIn):
         if index == 0:
             continue
-        del dictionary["l"]
-        del dictionary["n"]
-        del dictionary["t"]
+        del dictionary["l"]  # low
+        del dictionary["n"]  # number of transactions
+        del dictionary["t"]  # time in unix msec
+
+        dictionary["ticker"] = dictionary.pop("T")
+        dictionary["volume"] = dictionary.pop("v")
+        dictionary["volumeWeighted"] = dictionary.pop("vw")
+        dictionary["open"] = dictionary.pop("o")
+        dictionary["close"] = dictionary.pop("c")
+        dictionary["high"] = dictionary.pop("h")
 
     return originalIn
 
 
 # a = cleanse_data(call_all_companies("2023-07-27"))
-call_ticker("a", 1)
+print(call_ticker_current("GOOG"))
