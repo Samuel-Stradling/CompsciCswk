3d1aa789c51e86f492347b440c340acc5552c82f
Macbook air <sammystradling2006@gmail.com>
2023-08-20 22:02:20 +0100

    changed the valueerror raised to connection error to distinguish between bad response and market closed. small logic fixes here and there. some formatting. simple docstrings for functinos. added insertdataintostockprices as a helper to insert into db. added update date statues to keep track of that table. finished backfill function and functionality largely

--- database-handling/autoBackfill.py
+++ database-handling/autoBackfill.py
@@ -2,33 +2,7 @@ import sqlite3
 
 
 def call_all_companies(date: str) -> list:
-    """
-    Retrieves data about companies from an API based on the given date.
-
-    Parameters:
-        date (str): The date in the format 'yyyy-mm-dd' for which data is requested.
-
-    Returns:
-        list: A list containing the date (element 0) and relevant company data as dictionaries (subsequent elements are dictionaries).
-
-    Raises:
-        Exception: if the API call fails or returns an error
-        ValueError: 'manually' raised for later use in the initialisation of the db
-
-    Dependencies:
-        - The function relies on the 'requests' module to make API calls.
-        - The function also depends on a 'companies' module that contains the 'company_dictionary'.
-        - The function requires the 'dotenv' module to load environment variables.
-
-    Note:
-        - Make sure to set up the 'api-token' environment variable with your API key.
-        - The function will filter for relevant companies based on the 'company_dictionary'.
-
-    Example:
-        >>> data = call_all_companies('2023-07-31')
-        >>> print(data)
-        ['2023-07-31', {'c': 100.2, 'h': 105.0, 'l': 99.5, 'o': 101.0, 't': 166726400000, 'n': 1200, 'v': 500000}]
-    """
+    """calls all companies and returns a list where the first element is the date"""
 
     import os
     import requests
@@ -42,7 +16,7 @@ def call_all_companies(date: str) -> list:
 
     rawData = requests.get(url)
     if rawData.status_code != 200:
-        raise ValueError(f"Request failed with status code {rawData.status_code})")
+        raise ConnectionError(f"Request failed with status code {rawData.status_code}")
 
     rawData = rawData.json()
 
@@ -61,12 +35,15 @@ def call_all_companies(date: str) -> list:
 
     return companySortedData
 
+
 def add_missing_dates() -> int:
+    """adds missing dates to the datestatuses table in the database"""
     from datetime import datetime, timedelta
+
     yesterday = (datetime.now() - timedelta(days=1)).date()
 
     try:
-        conn = sqlite3.connect("data\main.sql")
+        conn = sqlite3.connect("data/main.sql")
         cursor = conn.cursor()
         cursor.execute("SELECT MAX(date) AS latestDate FROM DateStatuses")
         latestDate = datetime.strptime(cursor.fetchone()[0], "%Y-%m-%d").date()
@@ -74,10 +51,12 @@ def add_missing_dates() -> int:
         if latestDate != yesterday:
             workingDate = latestDate + timedelta(days=1)
             while workingDate <= yesterday:
-                cursor.execute("INSERT INTO DateStatuses (date, complete_data, market_open) VALUES (?, ?, ?)", (str(workingDate), False, True))
+                cursor.execute(
+                    "INSERT INTO DateStatuses (date, complete_data, market_open) VALUES (?, ?, ?)",
+                    (str(workingDate), False, True),
+                )
                 workingDate += timedelta(days=1)
             conn.commit()
-
 
     except sqlite3.Error as error:
         print("Error: {}".format(error))
@@ -93,11 +72,12 @@ def add_missing_dates() -> int:
 
 
 def find_last_full_date() -> str:
+    """finds the last date with a full set of data"""
     try:
-        conn = sqlite3.connect("data\main.sql")
+        conn = sqlite3.connect("data/main.sql")
         cursor = conn.cursor()
         cursor.execute(
-            "SELECT MIN(date) AS oldestCompleteDate FROM DateStatuses WHERE complete_data = true;"
+            "SELECT MAX(date) AS oldestCompleteDate FROM DateStatuses WHERE complete_data = true;"
         )
         result = cursor.fetchone()
 
@@ -116,16 +96,81 @@ def find_last_full_date() -> str:
         return result[0]
 
 
+def insert_data_into_stockprices(data: list):
+    """inserts data returned from call_all_companies into the stockprices table"""
+    try:
+        conn = sqlite3.connect("data/main.sql")
+        cursor = conn.cursor()
+        date = data[0]
+        for company in data:
+            if company == date:
+                continue
+            insertArgs = (
+                company["T"],
+                date,
+                company["o"],
+                company["c"],
+                company["h"],
+                company["v"],
+                company["vw"],
+            )
+            cursor.execute(
+                "INSERT INTO StockPrices (ticker, date, open, close, high, volume, weighted_volume) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                insertArgs,
+            )
+            conn.commit()
+
+    except sqlite3.Error as error:
+        print("Error: {}".format(error))
+        return -1
+
+    finally:
+        # Close the cursor and connection
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+        return 0
+    
+def update_date_statuses(date: str):
+    """updates the date status table based on the stockprices table"""
+    try:
+        conn = sqlite3.connect("data/main.sql")
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT COUNT(*) FROM StockPrices WHERE date = ?", (date,))
+        count = cursor.fetchone()[0]
+        if count >= 90: # IDK WHY SOME OF THE RESPONSES DO NOT HAVE ALL COUNTRIES??
+            cursor.execute("UPDATE DateStatuses SET complete_data = ? WHERE date = ?", (True, date))
+        else:
+            cursor.execute("UPDATE DateStatuses SET market_open = ? WHERE date = ?", (False, date))
+        
+        conn.commit()
+
+    except sqlite3.Error as error:
+        print("Error: {}".format(error))
+        return -1
+
+    finally:
+        # Close the cursor and connection
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+        return 0
+
+
 def backfill(lastFullDate: str) -> int:
+    """responsible for controlling all of the backfilling"""
     import time
     from datetime import timedelta, datetime
 
-    
+
     yesterday = datetime.now() - timedelta(days=1)
 
     if lastFullDate != yesterday:
         datesToFill = []
-        startDate = (datetime.strptime(lastFullDate, "%Y-%m-%d")).date()
+        startDate = (datetime.strptime(lastFullDate, "%Y-%m-%d") + timedelta(days=1)).date()
         yesterday = (datetime.now() - timedelta(days=1)).date()
 
         currentDate = startDate
@@ -134,11 +179,26 @@ def backfill(lastFullDate: str) -> int:
             currentDate += timedelta(days=1)
         datesToFill.append(str(yesterday))
 
-       
     for date in datesToFill:
-        print(date)
-            
+        try:
+            time.sleep(12)
+            data = call_all_companies(date)
+            print("item count:", len(data)-1)
+            insert_data_into_stockprices(data)
+            update_date_statuses(date)
+            print(date)
+        except ValueError:
+            update_date_statuses(date)
+            print(date)
+        except ConnectionError:
+            print(date, "not working for some reason")
+            continue
 
 
-# backfill(find_last_full_date())
-add_missing_dates()
+backfill(find_last_full_date())
+# backfill("2023-08-17")
+# insert_data_into_stockprices(call_all_companies("2023-08-17"))
+# update_date_statuses("2023-08-17")
+
+
+# add_missing_dates()
