c710133e3c6669b6f579789719e186d48809e7bf
Sam Lee <sammystradling2006@gmail.com>
2023-09-25 19:43:34 +0100

    merge_sort finished and working

--- database-handling/sort.py
+++ database-handling/sort.py
@@ -157,7 +157,76 @@ class SortItems:
         #     print("worked")
 
     def merge_sort(self):
-        pass
+        """principle: adjacent sublists are merged in order, until there is only 1 ordered sublist."""
+
+        def merge(lst: list, sortMetric: str) -> list:
+            """merges two given sub lists in order size"""
+            leftSubList = lst[0]
+            rightSubList = lst[1]
+            combinedLen = len(leftSubList) + len(rightSubList)
+            newList = []
+            while len(newList) != combinedLen:
+                if len(leftSubList) == 0:
+                    for remaining in rightSubList:
+                        newList.append(remaining)
+                    return newList
+                if len(rightSubList) == 0:
+                    for remaining in leftSubList:
+                        newList.append(remaining)
+                    return newList
+
+
+                if leftSubList[0][sortMetric] < rightSubList[0][sortMetric]:
+                    newList.append(leftSubList.pop(0))
+                elif leftSubList[0][sortMetric] > rightSubList[0][sortMetric]:
+                    newList.append(rightSubList.pop(0))
+                elif leftSubList[0][sortMetric] == rightSubList[0][sortMetric]:
+                    newList.append(leftSubList.pop(0))
+                    newList.append(rightSubList.pop(0))
+
+            return newList
+
+        def controller(sortMetric, toBeSorted):
+
+
+            newList =[]
+
+            if len(toBeSorted) % 2 != 0:
+                #odd number of items
+                index = 0
+                while not (len(newList) == len(toBeSorted)//2 + 1):
+                    newList.append(merge([toBeSorted[index], toBeSorted[index+1]], sortMetric))
+                    if index + 2 != len(toBeSorted) - 1:
+                        #if end of list not reached
+                        index += 2
+                    else:
+                        newList.append(toBeSorted[index+2]) #append the remaining value
+                return newList
+
+            else:
+                # even values in list path
+                index = 0
+                while not (len(newList) == len(toBeSorted)/2):
+                    newList.append(merge([toBeSorted[index], toBeSorted[index+1]], sortMetric))
+                    if index + 2 != len(toBeSorted) - 1:
+                        index += 2
+                    else:
+                        newList.append(toBeSorted[index+2])
+                return newList
+
+        values = [[x] for x in self.values]
+        while len(values[0]) != len(self.values):
+            values = controller(self.sortMetric, values)
+
+        return values[0]
+
+
+
+
+    
+        
+
+        
 
     def quick_sort(self):
         pass
@@ -168,10 +237,11 @@ class SortItems:
 
 sorter = SortItems(
     sortMethod="bubble",
-    sortMetric="volume",
-    startDate="2023-09-08",
+    sortMetric="high",
+    startDate="2023-09-11",
     endDate="2023-09-13",
 )
-sorter.bubble_sort()
-for result in sorter.values:
-    print(result)
+for value in sorter.merge_sort():
+    print(value)
+
+
