f62951d81ea735541e03c50b5ce428e44ae078ed
Samuel-Stradling <sammystradling2006@gmail.com>
2023-11-12 17:51:07 +0000

    made the backfill asynchronous

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-
+import asyncio
+from DatabaseHandling.autoBackfill import backfill, find_last_full_date
 from gui.master import tkinterApp
 
-app = tkinterApp()
-app.geometry("900x900")
-app.mainloop()
+async def perform_backfill():
+    await asyncio.sleep(0)  # Allows other tasks to run, just a placeholder
+    backfill(find_last_full_date())
 
+async def main():
+    backfill_task = asyncio.create_task(perform_backfill())
+
+    app = tkinterApp()
+    app.geometry("900x900")
+    app.mainloop()
+
+    await backfill_task  # Wait for the backfill task to complete
+
+if __name__ == "__main__":
+    asyncio.run(main())
