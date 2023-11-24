259080fee189f83a168c996c131a3c930b8e1443
Samuel-Stradling <sammystradling2006@gmail.com>
2023-11-24 12:26:46 +0000

    swapped async functions so that backfill is carried out before screen displayed

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import asyncio
 from DatabaseHandling.autoBackfill import backfill, find_last_full_date
 from gui.master import tkinterApp
 
-async def perform_backfill():
+async def display_gui():
     await asyncio.sleep(0)  # Allows other tasks to run, just a placeholder
-    backfill(find_last_full_date())
-
-async def main():
-    backfill_task = asyncio.create_task(perform_backfill())
-
     app = tkinterApp()
     app.geometry("900x900")
     app.title("Finance Analysis App")
     app.mainloop()
 
-    await backfill_task  # Wait for the backfill task to complete
+async def main():
+    gui_task = asyncio.create_task(display_gui())
+    print("Please wait for the backfill of the database to finish..")
+    backfill(find_last_full_date())
+
+    await gui_task  # Wait for the backfill task to complete
 
 if __name__ == "__main__":
     asyncio.run(main())
